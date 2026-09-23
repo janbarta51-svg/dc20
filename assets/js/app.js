a2a68a301d8007f9533b1a79e299fb2d9a49200d
janbarta51-svg <jan.barta51@gmail.com>
2026-09-23 08:46:54 +0200

    Align app version with chat media release

--- assets/js/app.js
+++ assets/js/app.js
@@ -1026,7 +1026,7 @@
   setTimeout(()=>splash?.classList.add('hide'),splashDelay);
   setTimeout(()=>splash?.remove(),splashDelay+650);
 
-  const APP_VERSION='2026.09.23.4';
+  const APP_VERSION='2026.09.23.5';
   let updatePromptShown=false;
 
   function showUpdatePrompt(){
