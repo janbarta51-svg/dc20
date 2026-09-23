18c3ff079e0976b1872a89f73e8a692da4d926e0
janbarta51-svg <jan.barta51@gmail.com>
2026-09-23 09:31:31 +0200

    Align app version with mobile polish

--- assets/js/app.js
+++ assets/js/app.js
@@ -1049,7 +1049,7 @@
   setTimeout(()=>splash?.classList.add('hide'),splashDelay);
   setTimeout(()=>splash?.remove(),splashDelay+650);
 
-  const APP_VERSION='2026.09.23.8';
+  const APP_VERSION='2026.09.23.9';
   let updatePromptShown=false;
 
   function showUpdatePrompt(){
