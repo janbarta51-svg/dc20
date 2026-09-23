48ed4b916f24ed0467d9e73f694f58eb99880e65
janbarta51-svg <jan.barta51@gmail.com>
2026-09-23 08:17:53 +0200

    Align app version with account release

--- assets/js/app.js
+++ assets/js/app.js
@@ -1026,7 +1026,7 @@
   setTimeout(()=>splash?.classList.add('hide'),splashDelay);
   setTimeout(()=>splash?.remove(),splashDelay+650);
 
-  const APP_VERSION='2026.09.23.2';
+  const APP_VERSION='2026.09.23.3';
   let updatePromptShown=false;
 
   function showUpdatePrompt(){
