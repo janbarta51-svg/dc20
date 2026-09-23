167845a6ac9f599728e1b55825075f9d4c0904a5
janbarta51-svg <jan.barta51@gmail.com>
2026-09-23 10:19:11 +0200

    Align app version with PWA scroll fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1049,7 +1049,7 @@
   setTimeout(()=>splash?.classList.add('hide'),splashDelay);
   setTimeout(()=>splash?.remove(),splashDelay+650);
 
-  const APP_VERSION='2026.09.23.10';
+  const APP_VERSION='2026.09.23.11';
   let updatePromptShown=false;
 
   function showUpdatePrompt(){
